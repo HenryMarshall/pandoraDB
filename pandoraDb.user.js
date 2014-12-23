aa5da0b4b1ae20698c349c7cabc953791611fb41
hbaughman <hbaughman741@gmail.com>
2014-12-23 12:48:22 -0500

    Initialized with greasemonkey config

---
+++ pandoraDb.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name        Pandora Scraper
+// @namespace   https://github.com/hbaughman
+// @include     http://www.pandora.com/*
+// @require     http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
+// @require     http://crypto-js.googlecode.com/svn/tags/3.0.2/build/rollups/md5.js"
+// @grant       none
+// @version     0.1
+// ==/UserScript==
