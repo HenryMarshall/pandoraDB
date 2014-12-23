7f3895a79653c6bfaa502c80fec812cd5607aef6
hbaughman <hbaughman741@gmail.com>
2014-12-23 13:02:47 -0500

    Scrape song information on update

--- pandoraDb.user.js
+++ pandoraDb.user.js
@@ -3,7 +3,26 @@
 // @namespace   https://github.com/hbaughman
 // @include     http://www.pandora.com/*
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
-// @require     http://crypto-js.googlecode.com/svn/tags/3.0.2/build/rollups/md5.js"
 // @grant       none
 // @version     0.1
 // ==/UserScript==
+
+var scraper = {
+  watchNowPlaying: function() {
+    $('.playerBarSong').on("DOMNodeInserted", function(data) {
+      var songInfo = scraper.getSongInfo();
+      console.log("songInfo: ",songInfo);
+    });
+  },
+
+  getSongInfo: function() {
+    var songInfo = {
+      title: $('.playerBarSong').text(),
+      artist: $('.playerBarArtist').text(),
+      album: $('.playerBarAlbum').text(),
+      station: $('.stationListItem.selected').find('.stationNameText').attr('title'),
+      liked: $('.thumbUpButton').hasClass('indicator')
+    };
+    return songInfo;
+  }
+}
