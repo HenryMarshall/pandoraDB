a67f677b9d7bf3148d14850e7dc2e890a0b18f8b
hbaughman <hbaughman741@gmail.com>
2014-12-24 18:30:38 -0500

    DB interface skeleton

--- pandoraDb.user.js
+++ pandoraDb.user.js
@@ -3,13 +3,14 @@
 // @namespace   https://github.com/hbaughman
 // @include     http://www.pandora.com/*
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
+// @require     http://crypto-js.googlecode.com/svn/tags/3.0.2/build/rollups/md5.js"
 // @grant       none
 // @version     0.1
 // ==/UserScript==
 
 var scraper = {
   watchNowPlaying: function() {
-    $('.playerBarSong').on("DOMNodeInserted", function(data) {
+    $('.playerBarAlbum').on("DOMNodeInserted", function(data) {
       var songInfo = scraper.getSongInfo();
       console.log("songInfo: ",songInfo);
     });
@@ -24,5 +25,32 @@ var scraper = {
       liked: $('.thumbUpButton').hasClass('indicator')
     };
     return songInfo;
+  }
+}
+
+var db = {
+
+  buildEntry: function(songInfo) {
+    this.title: songInfo.title,
+    this.artist: songInfo.artist,
+    this.album: songInfo.album,
+    this.liked: [],
+    this.playCount: { _total: 0 }
+  },
+
+  generateSongId: function(virginEntry) {
+    return CryptoJS.MD5(virginEntry);
+  },
+
+  getEntry: function(virginEntry) {
+    var id = generateSongId(virginEntry),
+        // Returns undefined if entry DNE.
+        getDbEntry = GM_getValue(id);
+
+    return getDbEntry ? $.parseJSON(getDbEntry) : virginEntry;
+  },
+
+  incrementEntry: function(entry) {
+    
   }
 }
